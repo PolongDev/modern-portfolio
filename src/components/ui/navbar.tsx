1743173a483cfcec302d32863a06bad8496887c0
Kenneth Gregorio Perez <148478983+PolongDev@users.noreply.github.com>
2024-05-01 17:52:46 +0800

    Update navbar.tsx

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -15,18 +15,18 @@ const Navbar: React.FC = () => {
   return (
     <nav className="sticky top-0 w-full h-14 md:h-16 bg-gray-950 flex flex-row items-center justify-between z-30 backdrop-filter backdrop-blur-md bg-opacity-30">
       <div className="flex flex-row items-center mx-4">
-        <a href="https://libyzxy0.com" className="text-2xl md:text-3xl font-semibold tracking-tight text-white flex flex-row">
-          <p className="text-sky-300">liby</p>zxy0
+        <a href="https://polongdev.com" className="text-2xl md:text-3xl font-semibold tracking-tight text-white flex flex-row">
+          <p className="text-sky-300">polong</p>dev
         </a>
       </div>
       <div className="flex flex-row items-center mx-3">
-        <a href="https://linkedin.com/in/libyzxy0" className="text-white text-xl md:text-2xl  mx-1.5 hover:text-sky-400 transition-all duration-300">
+        <a href="https://linkedin.com/in/polongdev" className="text-white text-xl md:text-2xl  mx-1.5 hover:text-sky-400 transition-all duration-300">
           <FaLinkedin />
         </a>
-        <a href="https://x.com/libyzxy0" className="text-white text-xl md:text-2xl mx-1.5 hover:text-sky-400 transition-all duration-300">
+        <a href="https://x.com/polongdev" className="text-white text-xl md:text-2xl mx-1.5 hover:text-sky-400 transition-all duration-300">
           <FaXTwitter />
         </a>
-        <a href="https://github.com/libyzxy0" className="text-white text-xl md:text-2xl mx-1.5 hover:text-sky-400 transition-all duration-300">
+        <a href="https://github.com/polongdev" className="text-white text-xl md:text-2xl mx-1.5 hover:text-sky-400 transition-all duration-300">
           <FaGithub />
         </a>
 
